3a76f534a65bcb76cd35af0b3144632302b28d31
Gustavo Lazarin <gustavolazarin03@gmail.com>
2023-11-27 20:04:49 -0300

    feat: add Loader

---
+++ client/src/components/Loader/Loader.tsx
@@ -0,0 +1,32 @@
+import styled, { keyframes } from 'styled-components';
+import { colors } from '../../styled-components/root';
+
+const spin = keyframes`
+  20% { background-size: 9px 60%, 9px 100%, 9px 100%; }
+  40% { background-size: 9px 80%, 9px 60%, 9px 100%; }
+  60% { background-size: 9px 100%, 9px 80%, 9px 60%; }
+  80% { background-size: 9px 100%, 9px 100%, 9px 80%; }
+`;
+
+const CustomLoader = styled.div`
+  --c: linear-gradient(${colors.purple} 0 0);
+  width: 45px;
+  height: 30px;
+  background: 
+    var(--c) 0%   100%,
+    var(--c) 50%  100%,
+    var(--c) 100% 100%;
+  background-size: 9px 100%;
+  background-repeat: no-repeat;
+  animation: ${spin} 1s infinite linear;
+`;
+
+function Loader() {
+    return (
+        <div>
+            <CustomLoader/>
+        </div>
+    );
+};
+
+export default Loader;
