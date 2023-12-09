728d52ca2df14f763dad4fc52cb21a2261692778
Bruno Moleta <brunomoleta@pm.me>
2023-12-09 18:38:58 -0300

    styles(fix)/Main resposivity

--- client/src/components/SingleProduct/styles.ts
+++ client/src/components/SingleProduct/styles.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components";
+import {genericValues} from "../../styled-components/root.ts";
 
 export const Div = styled.div`
   display: flex;
   flex-direction: column;
   gap: 10px;
   width: 100%;
-  max-width: 100svw;
+  max-width: ${genericValues.genericMaxWidth};
 `;
