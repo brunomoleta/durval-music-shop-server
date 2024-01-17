071bf4c56a3492211193e184158a1b126e486b92
Bruno Moleta <brunomoleta@pm.me>
2024-01-17 15:43:26 -0300

    Update unit test for product card
    The unit test for rendering a product card has been updated to its price, brand and name. The use of `priceString` function from `services/utils.ts` has been introduced for price-related checks.

--- client/src/tests/unit/renderProduct.route.spec.tsx
+++ client/src/tests/unit/renderProduct.route.spec.tsx
@@ -4,9 +4,11 @@ import { product } from "../mocks/create/renderProduct.route.mock.ts";
 import CardProduct from "../../components/CardProduct";
 import { UserProvider } from "../../providers/UserContext";
 import { BrowserRouter } from "react-router-dom";
+import {priceString} from "../../services/utils.ts";
 
 describe("ProductCard", () => {
-  test("Should render product card", () => {
+
+  test("Should render product card name, price and brand", () => {
     const guitar = product.guitar;
 
     render(
@@ -19,5 +21,10 @@ describe("ProductCard", () => {
     expect(screen.getByText(guitar.name).textContent).toBe(
       "Fender Stratocaster sunburst",
     );
+
+    expect(screen.getByText(priceString(guitar.price)).textContent).toBe(
+      priceString(7000),
+    );
+    expect(screen.getByText(guitar.brandName).textContent).toBe("fender");
   });
 });
