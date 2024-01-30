eb0c5cac7df297daf88b6b1d66e0c8708083ef83
Bruno Moleta <brunomoleta@pm.me>
2024-01-30 19:39:50 -0300

    Remove styling for anchor tags
    The anchor tag styling was removed from the  GlobalStyles.tsx file.

--- client/src/styled-components/GlobalStyles.tsx
+++ client/src/styled-components/GlobalStyles.tsx
@@ -123,6 +123,7 @@ const GlobalStyles = createGlobalStyle`
     font-weight: inherit;
   }
 
+
   h1 {
     font-size: inherit;
     font-weight: inherit;
@@ -204,12 +205,6 @@ const GlobalStyles = createGlobalStyle`
     max-inline-size: none;
   }
 
-  a {
-    // pointer-events: none;
-    cursor: pointer;
-    text-decoration: none;
-    color: black;
-  }
 
   /* Chrome, Safari, Edge, Opera */
   input::-webkit-outer-spin-button,
