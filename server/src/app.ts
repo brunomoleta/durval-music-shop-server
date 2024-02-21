3871631eb07c1760e9ab8cb7de2989e2069dc6af
Bruno Moleta <brunomoleta@pm.me>
2024-02-21 11:57:26 -0300

    Refactor main script link and expand CORS settings
    The client script source has been modified in the index.html, now referencing main.tsx instead of main.js script. Additionally, improved server-side CORS settings by expanding allowed origins to include Netlify.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,8 @@
 import "express-async-errors";
-import express, { Application, json } from "express";
-import { PrismaClient } from "@prisma/client";
-import { handleErrors } from "./middlewares/handleErrors.middleware";
-import { router } from "./routers/index.router";
+import express, {Application, json} from "express";
+import {PrismaClient} from "@prisma/client";
+import {handleErrors} from "./middlewares/handleErrors.middleware";
+import {router} from "./routers/index.router";
 import cors from "cors";
 
 export const app: Application = express();
@@ -10,15 +10,16 @@ export const app: Application = express();
 export const prisma = new PrismaClient();
 
 const ngrokPattern = /https:\/\/[a-zA-Z0-9-]+\.ngrok-free\.app(.*)/;
-const regexPattern: RegExp = /.durvalmusicshop.*\.vercel\.app.*/;
+const vercel: RegExp = /.durvalmusicshop.*\.vercel\.app.*/;
+const netlify: RegExp = /.durvalmusicshop.*\.netlify\.app.*/;
 const localUrl: string = "http://localhost:5173";
-const acceptedValuesArray = [regexPattern, localUrl, ngrokPattern]
+const acceptedValuesArray = [netlify, localUrl, ngrokPattern, vercel]
 
 const corsOptions = {
-  origin: acceptedValuesArray,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
+    origin: acceptedValuesArray,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
