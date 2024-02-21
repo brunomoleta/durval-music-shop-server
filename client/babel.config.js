94bc7d48e78b7bcf7ffcda3cac939a9bf6988d03
Bruno Moleta <brunomoleta@pm.me>
2024-02-21 16:08:21 -0300

    package/return to the previous package.json

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  presets: [["@babel/preset-env"]],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+          },
+        modules: "commonjs",
+      },
+    ],
+    ["@babel/preset-react", { runtime: "automatic" }],
+
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    "@babel/plugin-transform-modules-commonjs",
+  ],
 };
